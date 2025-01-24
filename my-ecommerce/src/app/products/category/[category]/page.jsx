7b15b2dfd3da7a0876a79fe3c30315c212057fd7
Student 1 <student1@Erxess-iMac-2.local>
2025-01-24 18:42:27 +0800

    my-ecommerce

--- my-ecommerce/src/app/products/category/[category]/page.jsx
+++ my-ecommerce/src/app/products/category/[category]/page.jsx
@@ -1,16 +1,16 @@
-import Card from "@/components/Card";
+import Card from "@/components/Card"
 
 export default async function Categories({ params }) {
-  const { category } = params;
+  const { category } = params
 
-  const products = await getProducts(category);
+  const products = await getProducts(category)
 
   const filteredCategories = products.filter((product) =>
     product.category.toLowerCase().includes(category.toLowerCase())
-  );
+  )
 
   if (!filteredCategories) {
-    return <p>NO product found in this category</p>;
+    return <p>NO product found in this category</p>
   }
 
   return (
@@ -21,15 +21,17 @@ export default async function Categories({ params }) {
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 async function getProducts(category) {
-  const res = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+  const res = await fetch(
+    `https://fakestoreapi.com/products/category/${category}`
+  )
 
   if (!res.ok) {
-    console.log("error");
+    console.log("error")
   }
 
-  return res.json();
+  return res.json()
 }
