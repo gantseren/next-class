eb17a4da2f12bcd8fa9559a181bf9bb0427b0bb9
Student 1 <student1@Erxess-iMac-2.local>
2025-01-24 19:26:33 +0800

    ecommerce

---
+++ ecommerce/src/app/singer/login/page.jsx
@@ -0,0 +1,80 @@
+"use client"
+
+import Cookies from "js-cookie"
+import React, { useState } from "react"
+
+export default function Register() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handelSubmit = (e) => {
+    e.preventDefault()
+
+    fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.token) {
+          Cookies.set("username", username, { expires: 7 })
+          Cookies.set("password", password, { expires: 7 })
+          Cookies.set("token", data.token, { expires: 7 })
+          alert("Login successfull! ")
+        } else {
+          alert("Login failed .")
+        }
+      })
+      .catch((error) => {
+        alert("An error occurred while logging in. Please try again later.")
+      })
+  }
+
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+      <form
+        onSubmit={handelSubmit}
+        className='bg-white p-6 rounded-lg shadow-md w-full max-w-sm'
+      >
+        <div className='mb-5'>
+          <label className='block mb-2 text-sm font-medium text-gray-900'>
+            Your email
+          </label>
+          <input
+            type='text'
+            name='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+            placeholder='Enter your email'
+          />
+        </div>
+        <div className='mb-5'>
+          <label className='block mb-2 text-sm font-medium text-gray-900'>
+            Your password
+          </label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+            placeholder='Enter your password'
+          />
+        </div>
+        <button
+          type='submit'
+          className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
---
+++ ecommerce/src/storage/Cart.jsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export default function Cart() {
+  const [cart, setCart] = useState([])
+
+  useEffect(() => {
+    const savedCart = JSON.parse(localStorage.getItem("shopping-cart")) || []
+    setCart(savedCart)
+  }, [])
+
+  const removeFromCart = (productId) => {
+    const updatedCart = cart.filter((product) => product.id !== productId)
+    setCart(updatedCart)
+    localStorage.setItem("shopping-cart", JSON.stringify(updatedCart))
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Cart</h1>
+      </div>
+      <ul className='max-w-md divide-y divide-gray-200 dark:divide-gray-700'>
+        {cart.length > 0 ? (
+          cart.map((product) => (
+            <li key={product.id} className='pb-3 sm:pb-4'>
+              <div className='flex items-center rtl:space-x-reverse'>
+                <div className='flex-1 min-w-0'>
+                  <p className='text-sm font-medium text-gray-900 truncate dark:text-white'>
+                    {product.title}
+                  </p>
+                  <p className='text-sm font-medium text-gray-900 truncate dark:text-white'>
+                    {product.price}
+                  </p>
+                </div>
+                <div>
+                  <button
+                    onClick={() => removeFromCart(product.id)}
+                    className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                    aria-label={`Remove ${product.title} from cart`}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))
+        ) : (
+          <p>No items in the cart</p>
+        )}
+      </ul>
+    </div>
+  )
+}
