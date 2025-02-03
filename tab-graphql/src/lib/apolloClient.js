16ed750c2b759459cbdc99101937fa2206c3c7a3
Student 1 <student1@Erxess-iMac-2.local>
2025-02-03 20:12:30 +0800

    typescript

--- tab-graphql/src/lib/apolloClient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
-
-const client = new ApolloClient({
-  uri: "https://ap-south-1.cdn.hygraph.com/content/cm6gb7z2o005g08wczx7h9ft2/master",
-  cache: new InMemoryCache(),
-})
-
-export default client
